0f27d6adf6a1f3c3b9e9193c88cf6d0c75291efa
Mbina <117299384+mobinaqabeigii@users.noreply.github.com>
2022-11-07 03:30:04 +0330

    p41

---
+++ p41.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std ;
+main () {
+	
+float x,y,h,f,k,o;
+   cin>>x>>y;
+   h=x+y;
+   cout<<"plus"<<h;
+   f=x-y;
+   cout<<"minus"<<f;
+   k=x*y;
+   cout<<"multiplication"<<k;
+   o=x/y;
+   cout<<"division"<<o;
+	
+	
+	
+	
+	
+	
+} 
